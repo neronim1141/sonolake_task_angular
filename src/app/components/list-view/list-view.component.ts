c311bbb85c6f9c6003899168d2ab5b06441459c4
Kacper Kruczek <neronim66@gmail.com>
2018-11-08 19:47:08 +0100

    paginator fix

--- src/app/components/list-view/list-view.component.ts
+++ src/app/components/list-view/list-view.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { CharactersGetService } from 'src/app/services/characters-get.service';
-import { HttpResponse } from '@angular/common/http';
-import { Character } from 'src/app/types/character.type';
+import { Component, OnInit } from "@angular/core";
+import { CharactersGetService } from "src/app/services/characters-get.service";
+import { HttpResponse } from "@angular/common/http";
+import { Character } from "src/app/types/character.type";
 
 @Component({
-  selector: 'sl-list-view',
-  templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+  selector: "sl-list-view",
+  templateUrl: "./list-view.component.html",
+  styleUrls: ["./list-view.component.scss"]
 })
 export class ListViewComponent implements OnInit {
   characters: Character[];
   page: number = 1;
   limit: number = 10;
-  q: string = '';
+  q: string = "";
   resourcesCount: number;
   constructor(private _charactersService: CharactersGetService) {}
 
@@ -37,7 +37,8 @@ export class ListViewComponent implements OnInit {
    * arrow function because we don't want that 'this' refer to Promise
    */
   private handleResponse = (res: HttpResponse<Character[]>): void => {
-    this.resourcesCount = parseInt(res.headers.get('x-total-count'));
+    this.resourcesCount = parseInt(res.headers.get("x-total-count"));
+    console.log(this.resourcesCount);
     this.characters = res.body;
   };
 }
